5b9f1b5c4fe3f503a8cd32075f750a33fce24cd4
Sami M Rahman <34039173+SamiMRahman@users.noreply.github.com>
2019-05-30 19:27:36 -0400

    Update Person4.java

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Sami Rahman
  *  @version 1.1
  */
 public class Person4 {
